e61614bfce866c24ab6dc0cbd57161572d8dfef1
Administrator <admin@example.com>
2019-03-23 17:49:13 +0800

    inits

--- FlyControl/lib/PIDv2.py
+++ FlyControl/lib/PIDv2.py
@@ -14,9 +14,9 @@ def engine_fly(_1553b,_1553a):
     y_last = 0.0
     z_last = 0.0
     #外环输入：欧拉角的积分参数
-    x_sum = 0.0
-    y_sum = 0.0
-    z_sum = 0.0
+    x_sum = [0.0]
+    y_sum = [0.0]
+    z_sum = [0.0]
     #外环输出：角速度期望值
     xv_et = 0.0
     yv_et = 0.0
@@ -26,9 +26,9 @@ def engine_fly(_1553b,_1553a):
     yv_last = 0.0
     zv_last = 0.0
     #内环输入：角速度的积分参数
-    xv_sum = 0.0
-    yv_sum = 0.0
-    zv_sum = 0.0
+    xv_sum = [0.0]
+    yv_sum = [0.0]
+    zv_sum = [0.0]
 
     while True:
         # 传感器测量的当前角度
@@ -81,17 +81,17 @@ def engine_outside_pid(et,et2,sum):
     palstance = 0.0
     if sum == None:
         #Z轴PID中只做P和D
-        palstance = cfg.kp * et + cfg.kd * (et - et2)
-        palstance = engine_limit_palstance(palstance)
+        palstance = [cfg.kp * et + cfg.kd * (et - et2)]
+        engine_limit_palstance(palstance)
         return palstance
-    sum += cfg.ki * et * 0.01
+    sum[0] += cfg.ki * et * 0.01
     #积分限幅
-    sum = engine_limit_palstance(sum)
+    engine_limit_palstance(sum)
     #XY轴PID反馈控制
-    palstance = cfg.kp * et + sum + cfg.kd * (et - et2)
+    palstance = [cfg.kp * et + sum[0] + cfg.kd * (et - et2)]
     #输出限幅
-    palstance = engine_limit_palstance(palstance)
-    return palstance
+    engine_limit_palstance(palstance)
+    return palstance[0]
 
 '''
 内环PID输入角速度输出PWM
@@ -106,40 +106,32 @@ def engine_inside_pid(et,et2,sum):
         pwm = cfg.v_kp * et + cfg.v_kd * (et -et2)
         engine_limit_pwm(pwm)
         return pwm
-    sum += cfg.v_ki * et * 0.01
+    sum[0] += cfg.v_ki * et * 0.01
     engine_limit_pwm(sum)
-    pwm = cfg.v_kp * et + sum + cfg.v_kd * (et - et2)
+    pwm = [cfg.v_kp * et + sum + cfg.v_kd * (et - et2)]
     engine_limit_pwm(pwm)
-    return pwm
+    return pwm[0]
 
 #外环角速度限幅
-def engine_limit_palstance(palstance):
+def engine_limit_palstance(val):
     MAX_PALSTANCE = 40  #允许的最大角速度（度/秒）
-    if palstance > MAX_PALSTANCE:
-        return MAX_PALSTANCE
-    elif 0 <= palstance <= MAX_PALSTANCE:
-        return palstance
-    elif -MAX_PALSTANCE < palstance < 0:
-        return palstance
-    elif palstance < -MAX_PALSTANCE:
-        return -MAX_PALSTANCE
+    if val[0] > MAX_PALSTANCE:
+        val[0] = MAX_PALSTANCE
+    elif val[0] < -MAX_PALSTANCE:
+        val[0] = -MAX_PALSTANCE
 
 
 #内环PWM限幅
 #实际限制的是油门0~100%,最
 def engine_limit_pwm(pwm):
     MAX_PWM = 100  # 油门大小：0~100%
-    if pwm > MAX_PWM:
-        return MAX_PWM
-    elif 0 <= pwm <= MAX_PWM:
-        return pwm
-    elif -MAX_PWM < pwm < 0:
-        return pwm
-    elif pwm < -MAX_PWM:
-        return -MAX_PWM
+    if pwm[0] > MAX_PWM:
+        pwm[0] = MAX_PWM
+    elif pwm[0] < -MAX_PWM:
+        pwm[0] = -MAX_PWM
 
 '''
 PWM发送到1553b总线，交给电机执行
 '''
 def motor_process(_1553b,x_pwm,y_pwm,z_pwm):
-    pass
+    print("x=%d,y=d%,z=d%"%(x_pwm,y_pwm,z_pwm))
