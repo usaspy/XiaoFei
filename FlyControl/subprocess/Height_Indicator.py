e29fcfac3e268b17c8445103e47d8cff70098abb
Administrator <admin@example.com>
2019-04-28 15:40:39 +0800

    inits

--- FlyControl/subprocess/Height_Indicator.py
+++
@@ -1,8 +0,0 @@
-'''
-测量无人机的离地高度
-
-'''
-
-import RPi.GPIO as GPIO
-import time
-
--- test/testpid.py
+++
@@ -1,8 +0,0 @@
-import time
-
-while True:
-    a = time.time()
-    time.sleep(0.1)
-    b= time.time()
-
-    print(b-a)
