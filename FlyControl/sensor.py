669736162377b6975152a2e6827ed381603a90cd
Administrator <admin@example.com>
2019-05-01 19:55:25 +0800

    inits

--- FlyControl/sensor.py
+++ FlyControl/sensor.py
@@ -9,9 +9,9 @@ def start(_1553b):
     thread_list = []
     try:
         t1 = threading.Thread(target=GY99.working, args=(_1553b,))
-        t2 = threading.Thread(target=sim7600.working, args=(_1553b,))
+        #t2 = threading.Thread(target=sim7600.working, args=(_1553b,))
         thread_list.append(t1)
-        thread_list.append(t2)
+        #thread_list.append(t2)
 
         for t in thread_list:
             t.setDaemon(True)
