be02b5822d22e89e0e5d59f41924c9704ba2c260
Administrator <admin@example.com>
2019-05-09 14:30:48 +0800

    inits

--- test/debug.py
+++ test/debug.py
@@ -4,12 +4,6 @@
 生成PID输出图形
 '''
 import matplotlib.pyplot as plt
-
-def output_to_datalog(data):
-    with open('d://data.txt', 'a') as f:
-        f.write(data + '\n')
-
-
 '''
 依据数据文件生成Chart曲线图
 Y1：
@@ -22,23 +16,28 @@ d2= []
 d3= []
 d4= []
 x= []
+
 def generate_pid_chart():
-    with open('d://data.txt', 'r') as f:
+    i = 0
+    with open('d://data.log', 'r') as f:
         lines = f.readlines()
         for line in lines:
             ls = line.split(',')
-            d1.append(ls[0])
-            d2.append(ls[1])
-            d3.append(ls[2])
-            d4.append(ls[3])
-            x.append(ls[4])
+            d1.append(int(ls[0]))
+            d2.append(int(ls[1]))
+            d3.append(int(ls[2]))
+            d4.append(int(ls[3]))
+            i+=1
+            x.append(i)
+        print(d1)
     plt.plot(x, d1, 'yo-')
     plt.plot(x, d2, 'go-')
     plt.plot(x, d3, 'bo-')
     plt.plot(x, d4, 'ro-')
-    plt.title('newspaper')
+    plt.title('test ROLL ')
     plt.grid()
     plt.tight_layout()
     plt.show()
+
 
 generate_pid_chart()
--- test/pynput_test.py
+++ test/pynput_test.py
@@ -1,11 +1,3 @@
-a= {'test':1}
+import time
 
-print(a.get('xxx','333'))
-
-
-a = [5*3+2+5+6*(1-0.2)]
-print(a)
-x_pwm=1
-y_pwm=2
-z_pwm=3
-print("x=%d y=%d z=%d"%(x_pwm,y_pwm,z_pwm))
+print(time.time_ns())
