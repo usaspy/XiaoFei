89047d04c707519d2a9663eae87cc7ea01efab3e
Administrator <admin@example.com>
2019-07-01 17:24:05 +0800

    inits

--- GroundStation/subprocess/Control_Link.py
+++ GroundStation/subprocess/Control_Link.py
@@ -12,8 +12,8 @@ import pyHook
 import pythoncom
 
 def working():
-        print("[Control_Link]打开地面站发射器...等待链接...")
-        vars.label_1.config(text="[Control_Link]打开地面站发射器,等待连接...")
+        print("[Control_Link]打开地面站接收器...等待链接...")
+        vars.label_1.config(text="[Control_Link]打开地面站接收器,等待连接...")
         try:
             sock_server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             sock_server.bind(('0.0.0.0',13130))
@@ -31,8 +31,8 @@ def working():
                 t.setDaemon(True)
                 t.start()
         except Exception as e:
-            vars.label_1.config(text="[Control_Link]地面站发射器发生异常...")
-            print("[Control_Link]地面站发射器发生异常...")
+            vars.label_1.config(text="[Control_Link]地面站接收器发生异常...")
+            print("[Control_Link]地面站接收器发生异常...")
         finally:
             vars.CONTROL_LINK_CLIENT = "未知"
             vars.CONTROL_LINK_STATUS = 0
@@ -41,6 +41,8 @@ def working():
 #控制链路服务器端
 #所有命令格式为“命令字串”
 def send_command(sock, addr):
+    while True:
+        time.sleep(5)
     hookmanager = pyHook.HookManager()
     def onKeyDown(event):
         try:
