2ed1298a3b9fab60457258a96af63e44ecad9c80
Administrator <admin@example.com>
2019-07-19 21:56:35 +0800

    inits

--- run_fc.py
+++ run_fc.py
@@ -17,11 +17,11 @@ import os
 if __name__ == "__main__":
     try:
         print("系统开机...")
-        #p1链接地面站控制链路进程
+        #p1 控制通信进程
         p1 = Process(target=communication.start,args=(_1553b,_1553a,),name='p1')
-        #p2传感器进程
+        #p2 姿态传感器进程
         p2 = Process(target=sensor.start,args=(_1553b,),name='p2')
-        #p3 马达动力进程
+        #p3 动力引擎进程
         p3 = Process(target=engine.init,args=(_1553b,_1553a,),name='p3')
 
         p1.daemon = True
@@ -37,9 +37,9 @@ if __name__ == "__main__":
         #affinity.set_process_affinity_mask(p3.pid, 7L)
         #以上方法在python3以后貌似失效了
         #可以用下面的方法来做
-        os.sched_setaffinity(p1.pid,[0x00])  #通信进程固定在CPU0上执行
-        os.sched_setaffinity(p2.pid,[0x01])  #传感器进程固定在CPU1上执行
-        os.sched_setaffinity(p3.pid,[0x02])  #动力进程固定在CPU2上执行
+        os.sched_setaffinity(p1.pid,[0x00])  #通信进程绑定在CPU-0上执行
+        os.sched_setaffinity(p2.pid,[0x01])  #传感器进程绑定在CPU-1上执行
+        os.sched_setaffinity(p3.pid,[0x02])  #动力进程绑定在CPU-2上执行
         #其中CPU 0 [1 2] 3 已禁用OS调度策略
 
         p1.join()
