61fd880f6977087853c31ad785736f0fcc1295a4
Administrator <admin@example.com>
2019-08-27 14:42:42 +0800

    inits

--- FlyControl/chips/AK8963.py
+++ FlyControl/chips/AK8963.py
@@ -6,38 +6,22 @@ import time
 注意：此程序要在MPU9255初始化完后执行
 '''
 class AK8963x(object):
-    I2C_SLV0_CTRL = 0x27
-    I2C_SLV0_ADDR = 0x25
-    I2C_SLV0_REG = 0x26
-    USER_CTRL = 0x6a
-
-    CNTL1 = 0x0A
-    BY_PASS = 0x37
+    AK8963_CNTL1 = 0x0A
     # 初始化AK8963芯片
     def __init__(self,bus,addr):
         self.bus = bus
         self.addr = addr
-        WHOAMI = bus.read_i2c_block_data(addr, 0x75, 1)
-        if WHOAMI[0] == 0x73:
-            # I2C Master方式读取地磁计
-            bus.write_byte_data(self.addr,self.I2C_SLV0_CTRL,0x86)    #  0x80 | 8 启用第一个从机配置,读取6byte数据。10000110=接收6个字节的磁力计数据     https://www.cnblogs.com/leptonation/p/5225889.html
-            bus.write_byte_data(self.addr,self.I2C_SLV0_REG,0x03)    #起始读取位置设置 0x00
-            bus.write_byte_data(self.addr,self.I2C_SLV0_ADDR,0x8c)    #Slave 0 I2C Address    AK8963_I2C_ADDR | 0x80
-            bus.write_byte_data(self.addr,self.USER_CTRL,0x20)    #I2C Master Mode Enable  启用
-            # bus.write_byte_data(self.addr,self.CNTL1,0x16)    #连续测量模式 100Hz，16bit Output
+        WHOAMI = bus.read_i2c_block_data(addr, 0x00, 1)
+        if WHOAMI[0] == 0x48:
+            #设置为连续测量模式 100Hz，16bit Output
+            bus.write_byte_data(self.addr, self.AK8963_CNTL1, 0x16)  #https://wenku.baidu.com/view/7247cf7a5bcfa1c7aa00b52acfc789eb172d9e5e.html
+            time.sleep(0.1)
+            print("AK8963已开启——Pass Through Mode")
 
-            # ByPass方式读取地磁计
-            #bus.write_byte_data(self.addr, self.BY_PASS, 0x02)  # 设置ByPass从0x0c设备读取地磁计数据
-            #time.sleep(0.1)
-            #bus.write_byte_data(self.addr, 0x0a, 0x12)  # 设置ByPass从0x0c设备读取地磁计数据
-            #time.sleep(0.1)
-
-            print("AK8963已设置为I2C Master模式")
-
-
-    # 获取加速度A值 x,y,z
-    def getMG(self):
-        #self.bus.write_byte_data(self.addr, self.I2C_SLV0_CTRL, 0x86)
-        data = self.bus.read_i2c_block_data(self.addr, 0x49, 6)
-        print(data)
-        return 1,1,1
+    # 获取地磁数据x,y,z
+    def getMAG(self):
+        mag = self.bus.read_i2c_block_data(self.addr, 0x03, 6)
+        mag_x = ((mag[1] << 8) | mag[0])
+        mag_y = ((mag[3] << 8) | mag[2])
+        mag_z = ((mag[5] << 8) | mag[4])
+        return mag_x,mag_y,mag_z
