3811b816075f68107628241115344cdc5140034d
Administrator <admin@example.com>
2019-09-03 17:24:10 +0800

    inits

--- FlyControl/subprocess/GY99_I2C.py
+++ FlyControl/subprocess/GY99_I2C.py
@@ -11,6 +11,7 @@
 
 '''
 import smbus
+import ctypes
 from FlyControl.chips.MPU9255 import MPU9255
 from FlyControl.chips.BMP280 import BMP280x
 from FlyControl.chips.AK8963 import AK8963x
@@ -37,12 +38,50 @@ def working(_1553b):
                     temp,press = BMP280.getTEMP_PRESS()   #摄氏度  千帕
                     count = 0
                 count = count + 1
+
                 print(round(acc_x,2),round(acc_y,2),round(acc_z,2), round(gyro_x,2),round(gyro_y,2),round(gyro_z,2),mag_x,mag_y,mag_z)
                 time.sleep(0.2)
+
             except Exception as e:
                 print("数据读取错误" + e)
     except Exception as e:
         print(e)
+
+
+#处理数据并写入_1553b数据总线
+def __resolve_data(acc_x,acc_y,acc_z,gyro_x,gyro_y,gyro_z,mag_x,mag_y,mag_z,_1553b):
+    ll = ctypes.cdll.LoadLibrary
+    lib = ll("/home/pi/libAHRS.so")
+
+    class StructPointer(ctypes.Structure):
+        _fields_ = [("roll", ctypes.c_float), ("yitch", ctypes.c_float), ("yaw", ctypes.c_float)]
+
+    lib.getAHRS.restype = ctypes.POINTER(StructPointer)
+
+    gx = ctypes.c_float(gyro_x)
+    gy = ctypes.c_float(gyro_y)
+    gz = ctypes.c_float(gyro_z)
+
+    ax = ctypes.c_float(acc_x)
+    ay = ctypes.c_float(acc_y)
+    az = ctypes.c_float(acc_z)
+
+    mx = ctypes.c_float(mag_x)
+    my = ctypes.c_float(mag_y)
+    mz = ctypes.c_float(mag_z)
+
+    dt = ctypes.c_float(0.002)
+
+    p = lib.getAHRS(gx, gy, gz, ax, ay, az, mx, my, mz, dt)
+
+    _1553b['GYRO_X'] = round(gyro_x, 2)
+    _1553b['GYRO_Y'] = round(gyro_y, 2)
+    _1553b['GYRO_Z'] = round(gyro_z, 2)
+
+    # 获取欧拉角 (度)
+    _1553b['ROLL'] = p.contents.roll
+    _1553b['PITCH'] = p.contents.yitch
+    _1553b['YAW'] = p.contents.yaw
 
 if __name__ == '__main__':
     working(None)
