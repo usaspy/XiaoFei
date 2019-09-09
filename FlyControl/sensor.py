0a5638b0ec63a970b0c3d420e364ad9c85159f33
Administrator <admin@example.com>
2019-09-09 13:42:39 +0800

    inits

--- FlyControl/sensor.py
+++ FlyControl/sensor.py
@@ -5,10 +5,10 @@ from FlyControl.subprocess import GY99
 import threading
 
 
-def start(_1553b):
+def start(_1553b,lock):
     thread_list = []
     try:
-        t1 = threading.Thread(target=GY99.working, args=(_1553b,))
+        t1 = threading.Thread(target=GY99.working, args=(_1553b,lock,))
         #t2 = threading.Thread(target=sim7600.working, args=(_1553b,))
         thread_list.append(t1)
         #thread_list.append(t2)
