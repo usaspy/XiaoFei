025a524d4c688c3945a9dc8dbe81381c0449b4f0
Administrator <admin@example.com>
2019-09-12 23:50:55 +0800

    补充提交，

--- FlyControl/subprocess/GY99.py
+++ FlyControl/subprocess/GY99.py
@@ -29,6 +29,39 @@ cmd7 = b'\xA5\x5A\x01\x00'
 # 恢复出厂设置
 cmd8 = b'\xA5\x5A\x02\x01'
 
+
+# 根据十六进制数转化为带符号的十进制数
+# 最高位是符号位，最高为位1是负数， 为0是正数
+def __hex2dec(d):
+    if d & 0x8000 == 0x8000:
+        d = d ^ 0xFFFF   #异或，相同为0，不同为1
+        d = ~d   #~符号表示运算：~d = -(d+1)
+    return d
+
+
+#处理数据并写入_1553b数据总线
+def __resolve_data(data,_1553b,lock):
+    #获取陀螺仪 (角度/秒) 默认量程2000  >>为什么除以16.4? 看http://www.openedv.com/forum.php?mod=viewthread&tid=80200&page=1
+    GYRO_X = (__hex2dec((data[4]<< 8) | data[5])) / 16.4
+    GYRO_Y = (__hex2dec((data[6] << 8) | data[7])) / 16.4
+    GYRO_Z = (__hex2dec((data[8] << 8) | data[9])) / 16.4
+
+        #获取欧拉角 (度)
+    ROLL = (__hex2dec((data[10]<< 8) | data[11])) / 100
+    PITCH = (__hex2dec((data[12] << 8) | data[13])) / 100
+    YAW = (__hex2dec((data[14] << 8) | data[15])) / 100
+
+    with lock:  # Manager默认是加了锁的，这里再加锁是为了同步更新以下这一批数据
+        _1553b['GYRO_X'] = round(GYRO_X,2)
+        _1553b['GYRO_Y'] = round(GYRO_Y,2)
+        _1553b['GYRO_Z'] = round(GYRO_Z,2)
+        _1553b['ROLL'] = ROLL
+        _1553b['PITCH'] = PITCH
+        _1553b['YAW'] = YAW
+        _1553b['Calibrated'] = data[16]   #已校准
+
+    #print(_1553b)
+
 def working(_1553b,lock):
     try:
         sr = serial.Serial(port=cfg.SERIAL_PORT_GY99, baudrate=115200, timeout=15, bytesize=serial.EIGHTBITS,
@@ -69,35 +102,3 @@ def working(_1553b,lock):
         print("[GY-99]通过串口[%s]获取飞控数据时发生异常..."% cfg.SERIAL_PORT_GY99)
     finally:
         sr.close()
-
-# 根据十六进制数转化为带符号的十进制数
-# 最高位是符号位，最高为位1是负数， 为0是正数
-def __hex2dec(d):
-    if d & 0x8000 == 0x8000:
-        d = d ^ 0xFFFF   #异或，相同为0，不同为1
-        d = ~d   #~符号表示运算：~d = -(d+1)
-    return d
-
-
-#处理数据并写入_1553b数据总线
-def __resolve_data(data,_1553b,lock):
-    #获取陀螺仪 (角度/秒) 默认量程2000  >>为什么除以16.4? 看http://www.openedv.com/forum.php?mod=viewthread&tid=80200&page=1
-    GYRO_X = (__hex2dec((data[4]<< 8) | data[5])) / 16.4
-    GYRO_Y = (__hex2dec((data[6] << 8) | data[7])) / 16.4
-    GYRO_Z = (__hex2dec((data[8] << 8) | data[9])) / 16.4
-
-        #获取欧拉角 (度)
-    ROLL = (__hex2dec((data[10]<< 8) | data[11])) / 100
-    PITCH = (__hex2dec((data[12] << 8) | data[13])) / 100
-    YAW = (__hex2dec((data[14] << 8) | data[15])) / 100
-
-    with lock:  # Manager默认是加了锁的，这里再加锁是为了同步更新以下这一批数据
-        _1553b['GYRO_X'] = round(GYRO_X,2)
-        _1553b['GYRO_Y'] = round(GYRO_Y,2)
-        _1553b['GYRO_Z'] = round(GYRO_Z,2)
-        _1553b['ROLL'] = ROLL
-        _1553b['PITCH'] = PITCH
-        _1553b['YAW'] = YAW
-        _1553b['Calibrated'] = data[16]   #已校准
-
-    #print(_1553b)
